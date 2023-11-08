55b6cf7441bf77481f312c49100a0b08e4d3dda6
12v <11772383+12v@users.noreply.github.com>
2023-11-08 11:26:44 +0000

    Add index.html

---
+++ docs/script.js
@@ -0,0 +1,22 @@
+function submitForm(event) {
+    event.preventDefault();
+    var input = document.getElementById('input').value.replace(/\s/g, '').toUpperCase();
+    fetch(`https://12v.github.io/postcode-lookup/postcodes/${input}.txt`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            return response.text()
+        })
+        .then(data => {
+            document.getElementById('result').innerHTML = 'Following the 2023 Review of Parliamentary constituencies, your constituency is:<br><strong> ' + data + '</strong>';
+        })
+        .catch(error => {
+            if (error.message == '404') {
+                document.getElementById('result').textContent = 'Your postcode was not found. Please check your postcode and try again.  Unfortunately, this tool only works for postcodes in England, Scotland and Wales.';
+            } else {
+                document.getElementById('result').textContent = 'An error occurred: ' + error.message;
+            }
+        });
+}
