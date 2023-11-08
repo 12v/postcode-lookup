4c32a292c55252a60151b91e149e32bd860cff80
12v <11772383+12v@users.noreply.github.com>
2023-11-08 15:17:42 +0000

    Tweak styles

--- docs/script.js
+++ docs/script.js
@@ -10,11 +10,11 @@ function submitForm(event) {
             return response.text()
         })
         .then(data => {
-            document.getElementById('result').innerHTML = 'Following the 2023 Review of Parliamentary constituencies, your constituency is:<br><strong> ' + data + '</strong>';
+            document.getElementById('result').innerHTML = 'Following the 2023 boundary review, your constituency is:<br><br><span class="result-text"> ' + data + '</span>';
         })
         .catch(error => {
             if (error.message == '404') {
-                document.getElementById('result').textContent = 'Your postcode was not found. Please check your postcode and try again.  Unfortunately, this tool only works for postcodes in England, Scotland and Wales.';
+                document.getElementById('result').textContent = 'Your postcode was not found, please check your postcode and try again.  Unfortunately, this tool currnetly only works for postcodes in England, Scotland and Wales.';
             } else {
                 document.getElementById('result').textContent = 'An error occurred: ' + error.message;
             }
